ca98227695100d5c5ec41a18cb2f026fb3773c09
iamanggi <anggipuspita452@gmail.com>
2024-12-13 15:56:01 +0700

    Menambahkan DetailUiState sebagai data model untuk mengelola state pada halaman detail mahasiswa

---
+++ app/src/main/java/com/example/prak8roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.prak8roomdatabase.ui.viewmodel
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUIEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
