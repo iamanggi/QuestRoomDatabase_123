6a8540dba514f9bf65b651b03630aba122a014ed
iamanggi <anggipuspita452@gmail.com>
2024-12-13 20:57:26 +0700

    Menambahkan PenyediaViewModel untuk mempermudah pembuatan ViewModel di aplikasi

--- app/src/main/java/com/example/prak8roomdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak8roomdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.prak8roomdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import com.example.prak8roomdatabase.KrsApp
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
@@ -10,6 +11,18 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer{
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer{
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 KrsApp().containerApp.repositoryMhs
             )
         }
