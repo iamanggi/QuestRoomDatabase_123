814cf829553743f52090df46fc0b8533a63cd0ac
iamanggi <anggipuspita452@gmail.com>
2024-12-13 14:10:12 +0700

    menambahkan AlamatNavigasi untuk mendefinisikan rute navigasi aplikasi, yaitu DestinasiHome, DestinasiDetail dan DestinasiUpdate dengan menggunakan argumen nim

--- app/src/main/java/com/example/prak8roomdatabase/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/prak8roomdatabase/ui/navigation/AlamatNavigasi.kt
@@ -2,4 +2,21 @@ package com.example.prak8roomdatabase.ui.navigation
 
 interface AlamatNavigasi {
     val route: String             //untuk menyimpan routing halaman
+
+}
+
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "${route}/{${NIM}}"
 }
