7f7dca3442de98b84d470eebe2473946d611d2da
iamanggi <anggipuspita452@gmail.com>
2024-12-13 15:58:50 +0700

    Menambahkan fungsi untuk Memindahkan data dari entity ke UI

--- app/src/main/java/com/example/prak8roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/prak8roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.prak8roomdatabase.ui.viewmodel
 
+import com.example.prak8roomdatabase.data.entity.Mahasiswa
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -11,4 +13,18 @@ data class DetailUiState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != MahasiswaEvent()
+}
+
+//DATA CLASS untuk menampung data yang akan ditampilkan di UI
+
+//Memindahkan data dari entity ke UI
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent{
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
