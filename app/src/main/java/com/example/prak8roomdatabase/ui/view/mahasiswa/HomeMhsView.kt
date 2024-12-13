ca5b4b1ff3b0181592cad42cf8f76d0deb9d460d
iamanggi <anggipuspita452@gmail.com>
2024-12-13 21:55:30 +0700

    Menambahkan fungsi HomeMhsView sebagai tampilan utama untuk halaman daftar/list mahasiswa.

--- app/src/main/java/com/example/prak8roomdatabase/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/prak8roomdatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -10,17 +10,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -28,10 +34,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.prak8roomdatabase.data.entity.Mahasiswa
+import com.example.prak8roomdatabase.ui.customwidget.TopAppBar
+import com.example.prak8roomdatabase.ui.viewmodel.HomeMhsViewModel
 import com.example.prak8roomdatabase.ui.viewmodel.HomeUiState
+import com.example.prak8roomdatabase.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier )
+{
+    Scaffold (topBar = {
+        TopAppBar(
+            judul = "Daftar Mahasiswa",
+            showBackButton = false,
+            onBack = { },
+        )
+    },
+        floatingActionButton = {
+        FloatingActionButton(
+            onClick = onAddMhs,
+            shape = MaterialTheme.shapes.medium,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Tambah Mahasiswa")
+        }
+    }){
+        innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 fun BodyHomeMhsView(
@@ -109,7 +156,6 @@ fun ListMahasiswa(
         )
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardMhs(
